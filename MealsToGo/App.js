56a0c7618948314eff411f1d3da58cddf2f789e3
Patrick P <patrickpetropoulos@gmail.com>
2022-04-03 00:07:27 -0400

    Create initial project

---
+++ MealsToGo/App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Writing first line of code in my App.</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
